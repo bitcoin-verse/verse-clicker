e82f25ea8856f5494f102fa1eb9c4eed451de901
Paul Bergamo <porlybe@gmail.com>
2023-09-19 14:58:14 +0900

    placeholder content for display

--- src/components/GameBoard/Display.tsx
+++ src/components/GameBoard/Display.tsx
@@ -1,7 +1,32 @@
 import React, { FC } from "react";
+import styled from "styled-components";
+
+const DisplayWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  gap: 16px;
+`;
+
+const CookieCount = styled.h2`
+  flex-basis: 100%;
+  text-align: center;
+`;
+
+const StatCount = styled.div`
+  font-weight: 600;
+`;
 
 const Display: FC = () => {
-  return <div>Top bar shows total, CPS & CPC</div>;
+  return (
+    <DisplayWrapper>
+      <CookieCount>Cookies: 0</CookieCount>
+      <StatCount>CPS: 0</StatCount>
+      <StatCount>CPC: 1.0</StatCount>
+    </DisplayWrapper>
+  );
 };
 
 export default Display;
