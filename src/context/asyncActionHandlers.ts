b21fbe74e9ffac982b042de35576c9074223d672
Paul Bergamo <porlybe@gmail.com>
2023-09-19 19:12:25 +0900

    Feature/api calls (#6)
    * add axios
    
    * add api calls
    
    * add save game to reducer
    
    * add save game action

--- src/context/asyncActionHandlers.ts
+++ src/context/asyncActionHandlers.ts
@@ -1,10 +1,19 @@
 import { Reducer } from "react";
 import { Action, State } from "./store";
 import { AsyncActionHandlers } from "use-reducer-async";
+import { getProgress, saveProgress } from "../api/progress";
 
-type AsyncActionSaveGame = { type: "SAVE_GAME"; save: string };
+type AsyncActionSaveGame = {
+  type: "SAVE_GAME";
+  payload: { address: string; progressBase64: string };
+};
 
-export type AsyncAction = AsyncActionSaveGame;
+type AsyncActionGetSaveGamve = {
+  type: "GET_SAVE";
+  payload: string;
+};
+
+export type AsyncAction = AsyncActionSaveGame | AsyncActionGetSaveGamve;
 
 export const asyncActionHandlers: AsyncActionHandlers<
   Reducer<State, Action>,
@@ -16,11 +25,35 @@ export const asyncActionHandlers: AsyncActionHandlers<
       try {
         dispatch({ type: "STARTED" });
 
-        console.log(action);
-        // some async action
-        // const response = await axios(action)....
+        const response = await saveProgress(
+          action.payload.address,
+          action.payload.progressBase64,
+        );
 
-        dispatch({ type: "GAME_SAVED", save: "" });
+        console.log("game save", response);
+        dispatch({
+          type: "GAME_SAVED",
+          payload: action.payload.progressBase64,
+        });
+      } catch (error) {
+        dispatch({ type: "FAILED", error: error as unknown as Error });
+      }
+    },
+
+  GET_SAVE:
+    ({ dispatch }) =>
+    async (action) => {
+      try {
+        dispatch({ type: "STARTED" });
+
+        const response = await getProgress(action.payload);
+
+        if (!response) throw new Error("Error getting response");
+
+        dispatch({
+          type: "GAME_SAVED",
+          payload: response,
+        });
       } catch (error) {
         dispatch({ type: "FAILED", error: error as unknown as Error });
       }
