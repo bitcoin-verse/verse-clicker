35262e7b3b12f14cf9ff48ce656629c82a4c6d79
Micah <micah@bitcoin.com>
2023-09-20 10:21:28 +0900

    make shop list

--- src/components/Shop/ShopList.tsx
+++ src/components/Shop/ShopList.tsx
@@ -1,7 +1,24 @@
 import React, { FC } from "react";
+import styled from "styled-components";
+import { Button } from "../Button";
+import game from "../../../src/game";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const ShopList: FC = () => {
-  return <div>THE SHOP List</div>;
+  const buildings = game.buildings.filter((building) => !building.locked);
+
+  return (
+    <Wrapper>
+      <h3>The Shop List</h3>
+      {buildings.map((building, i) => (
+        <Button key={i}>{building.name}</Button>
+      ))}
+    </Wrapper>
+  );
 };
 
 export default ShopList;
