81b490d67d1e342c3b0f1dd4203ec271f1d30c8c
Paul Bergamo <porlybe@gmail.com>
2023-09-20 10:09:39 +0900

    remove error catching from api, let async handlers do that

--- src/api/progress.ts
+++ src/api/progress.ts
@@ -2,27 +2,18 @@ import axios from "axios";
 import { PROGRESS } from "./endpoints";
 
 export const getProgress = async (address: string) => {
-  try {
-    const { data } = await axios.get<string>(`${PROGRESS},${address}`);
+  const { data } = await axios.get<string>(`${PROGRESS}/${address}`);
 
-    console.log(data);
+  console.log(data);
 
-    return data;
-  } catch (error) {
-    console.log("error getting progress", error);
-  }
+  return data;
 };
 
 export const saveProgress = async (address: string, progressBase64: string) => {
-  try {
-    const { data } = await axios.post(PROGRESS, {
-      address,
-      progressBase64,
-    });
+  const { data } = await axios.post(PROGRESS, {
+    address,
+    progressBase64,
+  });
 
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log("error saving progress", error);
-  }
+  return data;
 };
