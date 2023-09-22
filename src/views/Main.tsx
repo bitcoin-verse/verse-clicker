b8b3a8de42431146048e6b501fb12bc22f4c1a5c
Micah <94164690+micahpatng@users.noreply.github.com>
2023-09-22 15:52:08 +0900

    connect wallet overlay (#37)
    * connect wallet overlay
    
    * fix click addition :scoot:
    
    * connect/loading overlay styling
    
    ---------
    
    Co-authored-by: Paul Bergamo <porlybe@gmail.com>

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -8,6 +8,8 @@ import useGameLoop from "../hooks/useGameLoop";
 import background from "../assets/background.png";
 import Leaderboard from "../components/Leaderboard";
 import { useTrackedState } from "../context/store";
+import { Web3Button } from "@web3modal/react";
+import { useAccount } from "wagmi";
 
 const GlobalStyle = createGlobalStyle`
   html, body, * {
@@ -17,37 +19,69 @@ const GlobalStyle = createGlobalStyle`
     font-family: Open-Sans, Helvetica, Sans-Serif;
     color: white;
   }
+  html,body{
+   overflow:hidden; 
+  }
 `;
 
-const PageWrapper = styled.div`
+const ContentsWrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+  box-sizing: border-box;
+  overflow: auto;
+  height: 100vh;
   width: 100%;
   padding: 16px;
   background-image: url(${background});
   background-attachment: fixed;
 `;
 
+const OverlayConnect = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: grid;
+  grid-template-rows: 70% 30%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(3, 12, 20, 0.5);
+  z-index: 1;
+`;
+
 const Main: FC = () => {
   useGameLoop();
 
   const { loading } = useTrackedState();
+  const { status } = useAccount();
 
   return (
     <>
       <GlobalStyle />
-      {loading && (
-        <div style={{ color: "black", textAlign: "center", padding: "1rem" }}>
-          LOADING...
-        </div>
-      )}
-      <PageWrapper>
+      <ContentsWrapper>
+        {(status !== "connected" || loading) && (
+          <OverlayConnect>
+            <div>
+              {status === "connected" ? (
+                <h1>Loading...</h1>
+              ) : (
+                <h1 style={{ margin: "2rem" }}>
+                  Connect Wallet to start
+                </h1>
+              )}
+
+              {status !== "connected" && <Web3Button />}
+            </div>
+          </OverlayConnect>
+        )}
         <Header />
         <GameBoard />
-        <Leaderboard />
+        {status === "connected" && <Leaderboard />}
         <Footer />
-      </PageWrapper>
+      </ContentsWrapper>
     </>
   );
 };
