634ffc294a497a9da47bcb6a98cbb566c885087d
Paul Bergamo <porlybe@gmail.com>
2023-09-22 13:59:04 +0900

    correctly calculate earned

--- src/context/reducers/recalculateCPS.ts
+++ src/context/reducers/recalculateCPS.ts
@@ -6,15 +6,15 @@ export type RecalculateCPSAction = { type: "RECALCULATE_CPS" };
 export const recalculateCPS = (state: State): State => {
   const { CPS, aMPC, newBuildings } = calculateTotalCPS(state.buildings);
 
-  let newCookies = state.player.cookies;
-  let newEarned = state.player.cookieStats.Earned;
+  let newCookies = 0;
+  let newEarned = 0;
 
   if (state.lastSave) {
     const now = Date.now() / 1000;
     const diffInSec = now - Number(state.lastSave);
 
     newCookies = state.player.cookies + diffInSec * CPS;
-    newEarned = state.player.cookies + diffInSec * CPS;
+    newEarned = state.player.cookieStats.Earned + diffInSec * CPS;
   }
 
   return {
