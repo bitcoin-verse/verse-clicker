b08c72f504686f93fb5d8be42eb037d9187d356e
Paul Bergamo <porlybe@gmail.com>
2023-09-22 11:51:43 +0900

    Fix/upgrades (#31)
    * make sure upgrade doesn't show if owned
    
    * fixing recalculate cps
    
    * moar calculations

--- src/context/reducers/recalculateCPS.ts
+++ src/context/reducers/recalculateCPS.ts
@@ -4,7 +4,7 @@ import { State } from "../store";
 export type RecalculateCPSAction = { type: "RECALCULATE_CPS" };
 
 export const recalculateCPS = (state: State): State => {
-  const CPS = calculateTotalCPS(state.player, state.buildings);
+  const { CPS, aMPC, newBuildings } = calculateTotalCPS(state.buildings);
 
   let newCookies = state.player.cookies;
   let newEarned = state.player.cookieStats.Earned;
@@ -24,9 +24,11 @@ export const recalculateCPS = (state: State): State => {
       ...state.settings,
       recalculateCPS: false,
     },
+    buildings: newBuildings,
     player: {
       ...state.player,
       aMPF: CPS / state.settings.frameRate,
+      aMPC,
       cookies: newCookies,
       cookieStats: {
         ...state.player.cookieStats,
