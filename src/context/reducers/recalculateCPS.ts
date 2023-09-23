58bf7bb203e0904cd4e0cd83ec468e91c40d9385
PorlyBe <porlybe@gmail.com>
2023-09-23 10:10:33 +0900

    some cleanup

--- src/context/reducers/recalculateCPS.ts
+++ src/context/reducers/recalculateCPS.ts
@@ -6,19 +6,15 @@ export type RecalculateCPSAction = { type: "RECALCULATE_CPS" };
 export const recalculateCPS = (state: State): State => {
   const { CPS, aMPC } = calculateTotalCPS(state.buildings);
 
-  let newCookies = 0;
-  let newEarned = 0;
-  let cookieDiff = 0;
-
   const aMPF = CPS / state.settings.frameRate;
+
+  let cookieDiff = 0;
 
   if (state.lastSaveLoaded) {
     const now = new Date();
     const then = new Date(state.lastSaveLoaded * 1000);
     const diff = Math.abs(now.getTime() - then.getTime()) / 1000;
     cookieDiff = diff * CPS;
-    newCookies = state.player.cookies + cookieDiff;
-    newEarned = state.player.cookieStats.Earned + newCookies;
   }
 
   return {
@@ -33,10 +29,10 @@ export const recalculateCPS = (state: State): State => {
       ...state.player,
       aMPC,
       aMPF,
-      cookies: newCookies,
+      cookies: state.player.cookies + cookieDiff,
       cookieStats: {
         ...state.player.cookieStats,
-        Earned: newEarned,
+        Earned: state.player.cookieStats.Earned + cookieDiff,
       },
     },
   };
