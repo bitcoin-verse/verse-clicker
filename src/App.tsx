a86b51aa519e9ac2395319cc9b3b884d7fb73105
PorlyBe <porlybe@gmail.com>
2023-09-24 13:20:06 +0900

    use infura

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from "react";
 import Main from "./views/Main";
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
+import { EthereumClient, w3mConnectors } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { goerli, mainnet, polygon } from "wagmi/chains";
 import { Provider } from "./context/store";
+import { infuraProvider } from "wagmi/providers/infura";
 
 const chains = [mainnet, polygon, goerli];
 const projectId = "8000cda0f00ad8e06049c5e030ddaa4c";
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const { publicClient } = configureChains(chains, [
+  infuraProvider({ apiKey: "1f47d876b0094053881ae761371be771" }),
+  // w3mProvider({ projectId }),
+]);
+
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
@@ -33,7 +34,7 @@ const App: FC = () => {
       <Web3Modal
         projectId={projectId}
         ethereumClient={ethereumClient}
-        defaultChain={goerli}
+        defaultChain={mainnet}
         themeMode="dark"
         themeVariables={{
           "--w3m-button-border-radius": "2rem",
