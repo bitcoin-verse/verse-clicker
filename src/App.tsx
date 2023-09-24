7f0a80133a68e892adc5c9bc57021efc1917577f
PorlyBe <porlybe@gmail.com>
2023-09-24 14:18:36 +0900

    switching around chains

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import {
 } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { goerli, mainnet, polygon } from "wagmi/chains";
+import { goerli, mainnet } from "wagmi/chains";
 import { Provider } from "./context/store";
 import { infuraProvider } from "wagmi/providers/infura";
 
 const projectId = "8000cda0f00ad8e06049c5e030ddaa4c";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [goerli, mainnet, polygon],
+  [goerli, mainnet],
   [
     infuraProvider({ apiKey: "1f47d876b0094053881ae761371be771" }),
     w3mProvider({ projectId }),
@@ -41,7 +41,7 @@ const App: FC = () => {
       <Web3Modal
         projectId={projectId}
         ethereumClient={ethereumClient}
-        defaultChain={mainnet}
+        defaultChain={goerli}
         themeMode="dark"
         themeVariables={{
           "--w3m-button-border-radius": "2rem",
