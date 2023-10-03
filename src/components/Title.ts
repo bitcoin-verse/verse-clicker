e4f3feb7f1d8d2a1834b400031e0899d40ccc1f2
Paul Bergamo <porlybe@gmail.com>
2023-10-03 17:15:56 +0900

    more moving

--- src/components/Title.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Title = styled.span`
-  color: #899bb5;
-`;
--- src/helpers/stringToBool.ts
+++
@@ -1,8 +0,0 @@
-export const stringToBool = (str: string) => {
-  switch (str) {
-    case "true":
-      return true;
-    case "false":
-      return false;
-  }
-};
