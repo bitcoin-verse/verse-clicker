e360c45bc1d2d8e3717751375eb3d5f28035647b
Paul Bergamo <porlybe@gmail.com>
2023-10-19 16:54:36 +0900

    use not connected in app and create useSocketEvents hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,62 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useTransition } from "react";
 
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { goerli, mainnet, polygon } from "wagmi/chains";
-import { createWeb3Modal } from "@web3modal/wagmi/react";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import { infuraProvider } from "wagmi/providers/infura";
-
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { ContextProvider } from "./context/store";
-
-import SocketCtxProvider from "./context/SocketContext";
 import Main from "./views/Main";
-
-const projectId = "1184cb8e8109ec7c4a9425c56b494e5e";
-
-const metadata = {
-  name: "VERSE Clicker",
-  description: "VERSE Clicker",
-  url: "https://clicker.verse.bitcoin.com",
-  icons: ["https://verse.bitcoin.com/images/favicon.png"],
-};
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, polygon, goerli],
-  [
-    infuraProvider({
-      apiKey: "1f47d876b0094053881ae761371be771",
-    }),
-  ],
-);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: [
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId,
-        showQrModal: false,
-        metadata,
-        // relayUrl: "wss://walletconnect-v2.ops.bitcoin.com",
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: { shimDisconnect: true },
-    }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: { appName: metadata.name },
-    }),
-  ],
-  publicClient,
-  webSocketPublicClient,
-});
-createWeb3Modal({
-  wagmiConfig,
-  projectId,
-  chains,
-
-  featuredWalletIds: [
-    "107bb20463699c4e614d3a2fb7b961e66f48774cb8f6d6c1aee789853280972c",
-    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
-  ],
-});
+import { useAccount, useNetwork } from "wagmi";
+import NotConnected from "./views/NotConnected";
+import { useDispatch } from "./context/store";
+import { useSocketCtx } from "./context/SocketContext";
+import useSocketEvents from "./hooks/useSocketEvents";
 
 const App: FC = () => {
+  const dispatch = useDispatch();
+
+  const { socket, isConnected: isSocketConnected } = useSocketCtx();
+  const { loading, setLoading } = useSocketEvents();
+
+  const { chain } = useNetwork();
+
+  const { status, address } = useAccount({
+    onConnect: ({ address: addr }) => {
+      if (!addr) return;
+      console.log("Web3 Connected");
+      setLoading(false);
+    },
+    onDisconnect: () => {
+      setLoading(true);
+      dispatch({ type: "RESET_GAME" });
+      console.log("Web3 Disconnected");
+    },
+  });
+
+  useEffect(() => {
+    if (status !== "connected" || !chain) return;
+    setLoading(true);
+
+    dispatch({ type: "RESET_GAME" });
+
+    if (
+      !(
+        chain.name === "Ethereum" ||
+        chain.name === "Polygon" ||
+        chain.name === "Goerli"
+      )
+    ) {
+      socket.disconnect();
+      return;
+    }
+    dispatch({ type: "SET_NETWORK", payload: chain.name });
+    socket.disconnect();
+    socket.connect();
+  }, [status, chain, address]);
+
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <SocketCtxProvider>
-        <ContextProvider>
-          <Main />
-        </ContextProvider>
-      </SocketCtxProvider>
-    </WagmiConfig>
+    <>
+      {status !== "connected" || loading || !isSocketConnected ? (
+        <NotConnected />
+      ) : (
+        <Main />
+      )}
+    </>
   );
 };
 
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,140 +1,28 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import Header from "../components/Header/Header";
-import styled, { createGlobalStyle } from "styled-components";
 import Footer from "../components/Footer";
 import GameBoard from "../components/GameBoard/GameBoard";
 import Particles from "../components/Particles";
 
-import { useAccount, useNetwork } from "wagmi";
+import { useTrackedState } from "../context/store";
 
-import { useSocketCtx } from "../context/SocketContext";
-import { useDispatch, useTrackedState } from "../context/store";
-import { Player } from "../context/reducers/player";
-import Loading from "../components/Loading";
-import { Leadeerboard } from "../context/reducers/leaderboard";
-import { BuildingData } from "../context/reducers/building";
 import WelcomeModal from "../components/WelcomeModal";
 
-const GlobalStyle = createGlobalStyle`
-  html, body, * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Barlow','Helvetica', sans-serif;
-    letter-spacing: 0em;
-  }
-
-  html, body, div {
-    color: white;
-  }
-
-  button {
-    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
-  }
-`;
-
-const ContentsWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  background: linear-gradient(
-      180deg,
-      #020a10 0%,
-      #10518d 78.65%,
-      #2975bd 93.75%,
-      #4c97dd 99.48%
-    ),
-    linear-gradient(0deg, #030c14, #030c14);
-  min-height: 100dvh;
-
-  @media (min-width: 768px) {
-    padding: 0 1rem;
-  }
-`;
+import Layout from "../components/Layout";
 
 const Main: FC = () => {
-  const { chain } = useNetwork();
-
-  const { socket, isConnected: isSocketConnected } = useSocketCtx();
-  const [loading, setLoading] = useState(true);
   const { player } = useTrackedState();
 
   const hasCookies = player.cookies > 1;
 
-  const dispatch = useDispatch();
-
-  const { address, isConnected, status } = useAccount({
-    onConnect: ({ address: addr }) => {
-      if (!addr) return;
-      console.log("Web3 Connected");
-    },
-    onDisconnect: () => {
-      setLoading(true);
-      dispatch({ type: "RESET_GAME" });
-      console.log("Web3 Disconnected");
-    },
-  });
-
-  useEffect(() => {
-    if (status !== "connected" || !chain) return;
-    setLoading(true);
-
-    dispatch({ type: "RESET_GAME" });
-
-    if (
-      !(
-        chain.name === "Ethereum" ||
-        chain.name === "Polygon" ||
-        chain.name === "Goerli"
-      )
-    ) {
-      socket.disconnect();
-      return;
-    }
-    dispatch({ type: "SET_NETWORK", payload: chain.name });
-    socket.disconnect();
-    socket.connect();
-  }, [status, chain, address]);
-
-  useEffect(() => {
-    const onInfo = (payload: Player) => {
-      setLoading(false);
-      dispatch({ type: "SET_PLAYER_DATA", payload });
-    };
-
-    const onLeaderboard = (payload: Leadeerboard) => {
-      dispatch({ type: "SET_LEADERBOARD", payload });
-    };
-
-    const onBuildingsData = (payload: BuildingData[]) => {
-      dispatch({ type: "UPDATE_BUILDINGS", payload });
-    };
-
-    socket.on("info", onInfo);
-    socket.on("leaderboard", onLeaderboard);
-    socket.on("buildings_data", onBuildingsData);
-
-    return () => {
-      socket.off("info", onInfo);
-      socket.off("leaderboard", onLeaderboard);
-      socket.off("buildings_data", onBuildingsData);
-    };
-  }, []);
-
   return (
-    <>
-      <GlobalStyle />
+    <Layout>
       <WelcomeModal />
-      {(status !== "connected" || loading || !isSocketConnected) && <Loading />}
-
-      <ContentsWrapper>
-        {isConnected && hasCookies && <Particles />}
-        <Header />
-        <GameBoard />
-        <Footer />
-      </ContentsWrapper>
-    </>
+      {hasCookies && <Particles />}
+      <Header />
+      <GameBoard />
+      <Footer />
+    </Layout>
   );
 };
 
