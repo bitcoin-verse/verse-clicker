473b1cb21f6fcf332a1403641fb276f0df622cbf
PorlyBe <porlybe@gmail.com>
2023-10-23 16:05:16 +0900

    create spinner icon

---
+++ src/components/Icons/Spinner.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import styled, { keyframes } from "styled-components";
+
+import spinner from "../../assets/spinner.png";
+
+const portalSpin = keyframes`
+  0% {
+    transform: rotate(359deg);
+  }
+`;
+
+const SpinnerWrapper = styled.div`
+  height: 4rem;
+  width: 4rem;
+  animation: ${portalSpin} 3s infinite linear;
+  animation-direction: reverse;
+`;
+
+const Spinner: FC = () => {
+  return (
+    <SpinnerWrapper>
+      <img src={spinner} alt="Loading Spinner" height="100%" width="100%" />
+    </SpinnerWrapper>
+  );
+};
+
+export default Spinner;
