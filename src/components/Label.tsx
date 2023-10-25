066f272192aa4ae7716a1701ac04735fd8dfd9c0
Micah <micah@bitcoin.com>
2023-10-25 16:25:49 +0900

    add $size on label

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
-export const Label = styled.span<{ $secondary?: boolean }>`
+export const Label = styled.span<{ $secondary?: boolean; $size?: string }>`
   color: ${({ $secondary }) => ($secondary ? "#899bb5" : "white")};
+  font-size: ${({ $size }) => ($size ? `${$size}rem` : "1rem")};
 `;
