2adc2a9c6d1acc061b76010b2e9809568b72b9ad
Paul Bergamo <porlybe@gmail.com>
2023-10-25 14:49:20 +0900

    Fix/particles (#84)
    * make header go over particles
    
    * tweak config opacity and create config method
    
    * set particles amount based on buildings unlocked

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -4,21 +4,15 @@ import Footer from "../components/Footer";
 import GameBoard from "../components/GameBoard/GameBoard";
 import Particles from "../components/Particles";
 
-import { useTrackedState } from "../context/store";
-
 import WelcomeModal from "../components/WelcomeModal";
 
 import Layout from "../components/Layout";
 
 const Main: FC = () => {
-  const { player } = useTrackedState();
-
-  const hasCookies = player.cookies > 1;
-
   return (
     <Layout>
+      <Particles />
       <WelcomeModal />
-      {hasCookies && <Particles />}
       <Header />
       <GameBoard />
       <Footer />
