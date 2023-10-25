3e2fdc31996b81ae932eed505f503d7b63dcef89
Paul Bergamo <porlybe@gmail.com>
2023-10-25 13:11:20 +0900

    set particles amount based on buildings unlocked

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,14 +1,22 @@
-import React, { FC, lazy, Suspense, useCallback } from "react";
+import React, { FC, lazy, Suspense, useCallback, useMemo } from "react";
 import type { Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
-import config from "./config";
+import { createConfig } from "./config";
+import { useTrackedState } from "../../context/store";
 
 const ReactParticles = lazy(() => import("react-particles"));
 
 const Particles: FC = () => {
+  const { buildings } = useTrackedState();
+
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
+
+  const buildingsCount = useMemo(() => {
+    const count = buildings.reduce((p, c) => (c.locked ? p : p + 1), 0) - 1;
+    return count;
+  }, [buildings]);
 
   return (
     <Suspense>
@@ -16,8 +24,9 @@ const Particles: FC = () => {
         id="particles"
         style={{
           position: "absolute",
+          zIndex: -1,
         }}
-        options={config}
+        options={createConfig({ particlesNumber: buildingsCount * 10 })}
         init={particlesInit}
       />
     </Suspense>
