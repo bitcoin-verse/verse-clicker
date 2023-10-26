869aedbb1f78a0c3538bb8f18dfbb484993afd5b
Micah <micah@bitcoin.com>
2023-10-26 17:25:57 +0900

    add flip props on chevronleft

--- src/components/Icons/ChevronLeft.tsx
+++ src/components/Icons/ChevronLeft.tsx
@@ -2,9 +2,10 @@ import React, { FC } from "react";
 
 interface Props {
   size?: number | string;
+  flip?: boolean;
 }
 
-const ChevronLeft: FC<Props> = ({ size = 20 }) => {
+const ChevronLeft: FC<Props> = ({ size = 20, flip = false }) => {
   return (
     <svg
       width={size}
@@ -12,10 +13,11 @@ const ChevronLeft: FC<Props> = ({ size = 20 }) => {
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: `${flip ? "rotate(180deg)" : "none"}` }}
     >
       <path
         d="M12.5 15L7.5 10L12.5 5"
-        stroke="#899BB5"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
