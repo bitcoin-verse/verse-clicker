e08991831de5ed880d3e16509d1e86abd07dfbad
PorlyBe <porlybe@gmail.com>
2023-10-26 16:07:55 +0900

    start putting stuff in settings

--- src/components/Sidebar/Settings/Settings.tsx
+++ src/components/Sidebar/Settings/Settings.tsx
@@ -1,7 +1,39 @@
 import React, { FC } from "react";
+import { Wrapper } from "./styled";
+import { useAccount, useChainId } from "wagmi";
+import { useDispatch, useTrackedState } from "../../../context/store";
+import { formatNumber } from "../../../helpers/formatNumber";
+import truncateEthAddress from "../../../helpers/truncateEthAddress";
 
 const Settings: FC = () => {
-  return <>settingsss</>;
+  const { address } = useAccount();
+  const chainId = useChainId();
+
+  const { player, settings } = useTrackedState();
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <div>Network: {chainId}</div>
+      <div>Address: {truncateEthAddress(address || "")}</div>
+      <div>{formatNumber(player.cookies)}</div>
+
+      <div>
+        <button
+          onClick={() => {
+            dispatch({
+              type: "SET_SETTINGS",
+              payload: { sound: !settings.sound },
+            });
+          }}
+        >
+          {settings.sound ? "Mute" : "Unmute"}
+        </button>
+
+        <button>Wipe Save (todo)</button>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Settings;
