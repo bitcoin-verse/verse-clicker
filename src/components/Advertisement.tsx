1e2366b942040bc7d9def052ee54791fd60a89c7
Micah <94164690+micahpatng@users.noreply.github.com>
2023-11-02 15:03:55 +0900

    [MTDB-11017] Update ads (#123)
    * add and update images
    
    * update links
    
    * update styling

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -1,44 +1,29 @@
 import React, { FC, useEffect, useState } from "react";
 
-import adBtc from "../assets/ad-btc.png";
-import adCorbin from "../assets/ad-corbin.jpg";
-import adGlobalists from "../assets/ad-globalists.jpg";
-import adClubHouse from "../assets/ad-club-house.png";
-import adBurnEngine from "../assets/ad-burn-engine.png";
-import adGoldenPepe from "../assets/ad-golden-pepe.png";
-import adMcdonalds from "../assets/ad-mcdonalds.png";
-import adNft from "../assets/ad-nft.png";
-import adUsername from "../assets/ad-username.png";
+import adLounge from "../assets/ad-lounge.png";
+import adPools from "../assets/ad-pools.png";
+import adSwap from "../assets/ad-swap.png";
+import adStaking from "../assets/ad-staking.png";
+import adFarms from "../assets/ad-farms.png";
 
 import styled from "styled-components";
 
+const baseUrl = process.env.REACT_APP_PUBLIC_URL;
+
 const adlist = [
-  { img: adBtc, link: "https://youtu.be/dQw4w9WgXcQ?si=oXRVClQuVONile0S" },
-  { img: adCorbin, link: "https://www.linkedin.com/in/corbinfraser/" },
+  { img: adLounge, link: `${baseUrl}lounge` },
   {
-    img: adGlobalists,
-    link: "https://youtu.be/8mMIocEGGM0?si=LhqsR_LcWocskpBt",
+    img: adPools,
+    link: `${baseUrl}pools/eth`,
   },
   {
-    img: adClubHouse,
-    link: "https://dex.dev.cloud.bitcoin.com/lounge/",
+    img: adSwap,
+    link: baseUrl,
   },
+  { img: adStaking, link: `${baseUrl}staking/eth/verse/` },
   {
-    img: adBurnEngine,
-    link: "https://bitcoin-portal.github.io/burn-srs-buy/src/index.html",
-  },
-  {
-    img: adGoldenPepe,
-    link: "https://youtu.be/vJMP7RBsoms?si=AI9rgrggncEDfAZ9",
-  },
-  {
-    img: adMcdonalds,
-    link: "https://www.bitcoin.com/jobs/",
-  },
-  { img: adNft, link: "https://main--chipper-hotteok-85cbb2.netlify.app/" },
-  {
-    img: adUsername,
-    link: "https://verse-usernames.vercel.app/",
+    img: adFarms,
+    link: `${baseUrl}farms/eth/`,
   },
 ];
 
@@ -52,7 +37,7 @@ const AdWrapper = styled.div<{ $mobileVersion?: boolean }>`
   display: ${({ $mobileVersion }) => ($mobileVersion ? "block" : "none")};
 
   @media (min-width: 768px) {
-    padding: 1rem 0;
+    padding: 1rem 1rem 1rem 0;
     display: ${({ $mobileVersion }) => ($mobileVersion ? "none" : "block")};
   }
 `;
