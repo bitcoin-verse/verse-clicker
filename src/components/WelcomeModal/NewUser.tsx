6c3b721a1422899de50a59482124f19d9472837b
catboop <cat@bitcoin.com>
2023-11-02 14:59:38 +0900

    add preliminary instructions

--- src/components/WelcomeModal/NewUser.tsx
+++ src/components/WelcomeModal/NewUser.tsx
@@ -9,6 +9,23 @@ const NewUser = () => {
         Click for verse, climb the leaderboard! Join the Verse community and
         experience a world of endless clicking fun.
       </Description>
+      <Description style={{ textAlign: 'left' }}>
+        Here is how to play:
+        <br />
+        <br />
+        1. Click the Verse moon to earn points
+        <br />
+        <br />
+        2. Boost your clicking power by holding, farming, staking, and/or
+        burning VERSE
+        <br />
+        <br />
+        3. Buy Buildings and Upgrades with your points to passively generate
+        more points
+        <br />
+        <br />
+        4. Keep clicking and buying to see the number go up!
+      </Description>
     </>
   );
 };
