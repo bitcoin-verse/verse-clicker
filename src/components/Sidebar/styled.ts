da142b207737c786efe95f2b65a2265c84c49a63
Paul Bergamo <porlybe@gmail.com>
2023-11-24 14:07:18 +0900

    style sidebar like boost buttons (#153)

--- src/components/Sidebar/styled.ts
+++ src/components/Sidebar/styled.ts
@@ -16,8 +16,55 @@ export const Wrapper = styled.div`
   right: 0;
 
   @media (min-width: 768px) {
-    left: 0;
+    left: 1rem;
     right: unset;
+    background: linear-gradient(0deg, #0f1823, #0f1823),
+      radial-gradient(
+        117.51% 1182.96% at 50% 50%,
+        #273953 0%,
+        rgba(0, 0, 0, 0) 100%
+      );
+    border: 1px solid;
+    border-image-source: radial-gradient(
+      117.51% 1182.96% at 50% 50%,
+      #273953 0%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    border-radius: 2rem;
+
+    &::before {
+      position: absolute;
+      content: "";
+      z-index: -1;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 2rem;
+      background: linear-gradient(0deg, #0f1823, #0f1823),
+        radial-gradient(
+          117.51% 1182.96% at 50% 50%,
+          #273953 0%,
+          rgba(0, 0, 0, 0) 100%
+        );
+    }
+
+    &::after {
+      content: "";
+      z-index: -2;
+      left: -1px;
+      right: -1px;
+      top: -1px;
+      bottom: -1px;
+      position: absolute;
+      border-radius: 2rem;
+      box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.4);
+      background: radial-gradient(
+        62.81% 328.16% at 50% 50%,
+        #26303f 0%,
+        rgba(59, 69, 90, 0) 100%
+      );
+    }
   }
 `;
 
@@ -37,7 +84,7 @@ export const SidebarButton = styled.button`
     left: 0;
     right: unset;
     padding: 1rem;
-    box-shadow: 0px 0px 2rem 0px #2fa9ee33;
+
     & > svg {
       height: 1.25rem;
       width: 1.25rem;
