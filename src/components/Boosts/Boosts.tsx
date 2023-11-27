320eaeeb4490bc5b6656f0d7797c02f4934b6bfe
Micah <micah@bitcoin.com>
2023-11-27 16:02:43 +0900

    hide burn and farm boosts when chain is polygon

--- src/components/Boosts/Boosts.tsx
+++ src/components/Boosts/Boosts.tsx
@@ -20,23 +20,31 @@ import {
 } from "./styled";
 import Check from "../Icons/Check";
 import Clock from "../Icons/Clock";
+import { useNetwork } from "wagmi";
 
-const boostList = (verseHolder: boolean, isFarmsOrStaking: boolean) => [
+const boostList = (
+  verseHolder: boolean,
+  isFarmsOrStaking: boolean,
+  isPolygon: boolean,
+) => [
   {
     id: "hold",
     unlocked: verseHolder,
+    show: true,
     label: "Hold",
     desciption: "10x clicks",
   },
   {
     id: "farm",
     unlocked: isFarmsOrStaking,
+    show: !isPolygon,
     label: "Farm",
     desciption: "2x production",
   },
   {
     id: "burn",
     unlocked: true,
+    show: !isPolygon,
     label: "Burn",
     desciption: "Skip time",
   },
@@ -72,34 +80,39 @@ const Boosts: FC<Props> = ({ mobileVersion }) => {
   const [content, setContent] = useState<string>();
   const { modalRef, showModal } = useModal();
   const { player } = useTrackedState();
+  const { chain } = useNetwork();
 
   const modalContent = getModalContent(content);
   const isFarmsOrStaking = player.isFarming || player.isStaking;
+  const isPolygon = chain?.id === 137;
 
   return (
     <Wrapper $mobileVersion={mobileVersion}>
       <Content>
         <H4>Boost your points</H4>
         <BoostTiles>
-          {boostList(player.verseHolder, isFarmsOrStaking).map((boost) => (
-            <BoostButton
-              key={boost.id}
-              onClick={() => {
-                setContent(boost.id);
-                showModal();
-              }}
-            >
-              <Label $unlocked={boost.unlocked} $cta={boost.id === "burn"}>
-                {boost.id === "burn" ? (
-                  <Clock size={16} />
-                ) : (
-                  <>{boost.unlocked ? <Check /> : <Lock />}</>
-                )}
-                {boost.label}
-              </Label>
-              <Boost $unlocked={boost.unlocked}>{boost.desciption}</Boost>
-            </BoostButton>
-          ))}
+          {boostList(player.verseHolder, isFarmsOrStaking, isPolygon).map(
+            (boost) =>
+              boost.show && (
+                <BoostButton
+                  key={boost.id}
+                  onClick={() => {
+                    setContent(boost.id);
+                    showModal();
+                  }}
+                >
+                  <Label $unlocked={boost.unlocked} $cta={boost.id === "burn"}>
+                    {boost.id === "burn" ? (
+                      <Clock size={16} />
+                    ) : (
+                      <>{boost.unlocked ? <Check /> : <Lock />}</>
+                    )}
+                    {boost.label}
+                  </Label>
+                  <Boost $unlocked={boost.unlocked}>{boost.desciption}</Boost>
+                </BoostButton>
+              ),
+          )}
         </BoostTiles>
       </Content>
       <Modal
