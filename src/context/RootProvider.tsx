c1aeb3191cf99c3bad0c874390b0e67d9b8d7efa
Paul Bergamo <porlybe@gmail.com>
2023-12-04 16:39:00 +0900

    cleanup

--- src/context/RootProvider.tsx
+++ src/context/RootProvider.tsx
@@ -26,11 +26,11 @@ const metadata: Web3ModalOptions["metadata"] = {
   url: "https://clicker.verse.bitcoin.com",
   icons: [
     `${
-      process?.env?.REACT_APP_PUBLIC_URL || "https://clicker.verse.bitcoin.com"
+      process?.env?.REACT_APP_PUBLIC_URL || "https://clicker.verse.bitcoin.com/"
     }verse-moon.png`,
   ],
 };
-console.log(metadata);
+
 const isDev = process.env.REACT_APP_DEV_ENV === "development";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
