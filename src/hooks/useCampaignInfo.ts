70b0eec313866f1029aa44ce6a70e711c3949870
Paul Bergamo <porlybe@gmail.com>
2023-12-06 17:46:23 +0900

    campaign info helper

--- src/hooks/useCampaignInfo.ts
+++ src/hooks/useCampaignInfo.ts
@@ -1,20 +1,68 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NetworkName } from "../context/reducers/network";
 import axios from "axios";
 
-const useCampaignInfo = (campaign: NetworkName) => {
-  useEffect(() => {
-    const getInfo = async () => {
-      const { data } = await axios.get(
-        `${
-          process.env.REACT_APP_WEBSOCKET_SERVER || "http://localhost:3001"
-        }/campaign/${campaign}`,
-      );
-      console.log(data);
-    };
+type CampaignInfo = { startDate: number; endDate: number };
 
+const useCampaignInfo = (campaign: NetworkName) => {
+  const [campaignInfo, setCampaignInfo] = useState<CampaignInfo>();
+  const [isActive, setIsActive] = useState<boolean>();
+
+  const getInfo = async () => {
+    const { data } = await axios.get<CampaignInfo>(
+      `${
+        process.env.REACT_APP_WEBSOCKET_SERVER || "http://localhost:3001"
+      }/campaign/${campaign}`,
+    );
+    setCampaignInfo(data);
+  };
+
+  useEffect(() => {
     getInfo();
   }, [campaign]);
+
+  useEffect(() => {
+    // start timer to auto switch the game to active (or inactive)
+    if (!campaignInfo) return;
+
+    // started
+    if (
+      Date.now() > campaignInfo.startDate &&
+      Date.now() < campaignInfo.endDate
+    ) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+
+    let timeout: NodeJS.Timeout;
+
+    // not started
+    if (Date.now() < campaignInfo.startDate) {
+      const msDiff = campaignInfo.startDate - Date.now();
+
+      timeout = setTimeout(() => {
+        getInfo();
+      }, msDiff);
+      return;
+    }
+
+    // finished
+    if (Date.now() < campaignInfo.endDate) {
+      const msDiff = campaignInfo.endDate - Date.now();
+
+      timeout = setTimeout(() => {
+        getInfo();
+      }, msDiff);
+      return;
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [campaignInfo]);
+
+  return { campaignInfo, isActive };
 };
 
 export default useCampaignInfo;
