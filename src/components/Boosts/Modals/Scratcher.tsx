c42608bc57707b787c533cc233caafe2bc3e6d4b
PorlyBe <porlybe@gmail.com>
2023-12-15 07:46:18 +0900

    add scratcher copy

--- src/components/Boosts/Modals/Scratcher.tsx
+++ src/components/Boosts/Modals/Scratcher.tsx
@@ -6,6 +6,7 @@ import { Description, ModalWrapper } from "../styled";
 import { H3 } from "../../H3";
 import { Text } from "../../Text";
 import { useTrackedState } from "../../../context/store";
+import WarningChip from "../../WarningChip";
 
 const Scratcher: FC = () => {
   const { isWallet } = useTrackedState();
@@ -27,6 +28,16 @@ const Scratcher: FC = () => {
         You will receive a one-time bonus equal to your current points
         production rate multiplied by your Scratch & Win prize amount
       </Text>
+      <Text $secondary>
+        NOTE: To receive a bonus you must
+        <br />
+        1. Purchase, scratch and claim with same address used here
+        <br />
+        2. Have a greater than zero production rate
+      </Text>
+      <WarningChip link="http://">
+        Click here for detailed instructions
+      </WarningChip>
       <LinkButton
         href={scratcherLink}
         {...(isWallet ? {} : { target: "_blank", rel: "noreferrer" })}
