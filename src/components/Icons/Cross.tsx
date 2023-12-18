dc355ef9b21f215bf9dc8747419516164b64563c
Micah <micah@bitcoin.com>
2023-12-18 10:46:30 +0900

    add pointer and onclick on cross svg

--- src/components/Icons/Cross.tsx
+++ src/components/Icons/Cross.tsx
@@ -2,9 +2,11 @@ import React, { FC } from "react";
 
 interface Props {
   size?: number | string;
+  pointer?: boolean;
+  onClick?: () => void;
 }
 
-const Cross: FC<Props> = ({ size = 16 }) => {
+const Cross: FC<Props> = ({ size = 16, pointer, onClick }) => {
   return (
     <svg
       width={size}
@@ -12,6 +14,8 @@ const Cross: FC<Props> = ({ size = 16 }) => {
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+      style={{ cursor: `${pointer && "pointer"}` }}
     >
       <path
         d="M14.5 1.50014L1 15M1.00004 1.5L14.5 14.9999"
