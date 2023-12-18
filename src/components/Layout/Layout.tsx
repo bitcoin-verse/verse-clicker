f7aa65e14836d659b0ef460d3e82f05c2bc45e22
Micah <micah@bitcoin.com>
2023-12-18 10:47:13 +0900

    display banner only before and during the campaign

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,15 +4,21 @@ import { AudioProvider } from "../../context/AudioProvider";
 import { useTrackedState } from "../../context/store";
 import { ThemeProvider } from "styled-components";
 import { themes } from "../themes";
+import ClickmasBanner from "../Banners/ClickmasBanner";
+import useCampaignInfo from "../../hooks/useCampaignInfo";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  const { gameMode: network } = useTrackedState();
+  const { gameMode: network, showCampaignBanner } = useTrackedState();
+  const { campaignPhase } = useCampaignInfo("Christmas");
 
   return (
     <ThemeProvider theme={themes[network]}>
       <AudioProvider>
         <ContentsWrapper>
           <GlobalStyle />
+          {showCampaignBanner && campaignPhase !== "AFTER" && (
+            <ClickmasBanner />
+          )}
           {children}
         </ContentsWrapper>
       </AudioProvider>
