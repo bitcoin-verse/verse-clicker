2d12022e33f79e9c47a97bb3899feec2401554ed
Micah <micah@bitcoin.com>
2024-01-15 11:45:04 +0900

    make banner component reusable

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from "styled-components";
 import { AudioProvider } from "../../context/AudioProvider";
 import { useTrackedState } from "../../context/store";
 import useCampaignInfo from "../../hooks/useCampaignInfo";
-import ClickmasBanner from "../Banners/ClickmasBanner";
+import Banner from "../Banners/Banner";
 import { themes } from "../themes";
 import { ContentsWrapper, GlobalStyle } from "./styled";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   const { gameMode: network, showCampaignBanner = true } = useTrackedState();
-  const { campaignPhase } = useCampaignInfo("Christmas");
+  const { campaignPhase } = useCampaignInfo("LunarNewYear");
 
   return (
     <ThemeProvider theme={themes[network]}>
@@ -18,7 +18,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
         <ContentsWrapper>
           <GlobalStyle />
           {showCampaignBanner && campaignPhase !== "AFTER" && (
-            <ClickmasBanner />
+            <Banner gameMode={network} />
           )}
           {children}
         </ContentsWrapper>
