f637bf61401e11a2ef748118a551a0c963b0d6fd
Micah <micah@bitcoin.com>
2024-01-15 15:23:17 +0900

    use json file for banner content

--- src/components/Banners/Banner.tsx
+++ src/components/Banners/Banner.tsx
@@ -1,15 +1,19 @@
 import React, { FunctionComponent } from "react";
+import { GameMode } from "src/context/reducers/network";
 
 import { useDispatch } from "../../context/store";
 import Cross from "../Icons/Cross";
 import { Link } from "../Link";
+import bannerJson from "./banner.json";
 import { Wrapper } from "./styled";
 
-const ClickmasBanner: FunctionComponent = () => {
-  const dispatch = useDispatch();
+interface Props {
+  gameMode: GameMode;
+}
 
-  const link =
-    "https://medium.com/@Bitcoin_Com/get-ready-for-clickmas-where-clicks-turn-to-rewards-1000-up-for-grabs-e87438a9772b";
+const Banner: FunctionComponent<Props> = ({ gameMode }) => {
+  const dispatch = useDispatch();
+  const campaign = bannerJson.find((banner) => banner.id === "LunarNewYear");
 
   const hideBanner = () => {
     dispatch({
@@ -21,9 +25,8 @@ const ClickmasBanner: FunctionComponent = () => {
   return (
     <Wrapper>
       <div>
-        🎅 Merry Clickmas! 🎅 Turn clicks into rewards — $1000 up for grabs!
-        Contest period: Dec 22 - Jan 5 👉{" "}
-        <Link href={link} target="_blank">
+        {campaign?.text}
+        <Link href={campaign?.link} target="_blank">
           Learn more
         </Link>
       </div>
@@ -32,4 +35,4 @@ const ClickmasBanner: FunctionComponent = () => {
   );
 };
 
-export default ClickmasBanner;
+export default Banner;
