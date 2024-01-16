e788a09152d2f4b877ff2d4f997611c0ad833282
Paul Bergamo <porlybe@gmail.com>
2024-01-16 16:55:25 +0900

    update urls to use existing env var

--- src/helpers/links.ts
+++ src/helpers/links.ts
@@ -1,8 +1,10 @@
 const isDev = process.env.REACT_APP_DEV_ENV === "development";
+const VERSE_BASE_URL =
+  process.env.REACT_APP_VERSE_BASE_URL || "https://verse.bitcoin.com/";
 
 export const generateScratcherUrl = (isWallet: boolean, campaign?: string) => {
-  const URL_PROD = `https://scratcher.verse.bitcoin.com`;
-  const URL_DEV = `https://dev.scratcher.verse.bitcoin.com`;
+  const URL_PROD = `https://scratcher.verse.bitcoin.com/`;
+  const URL_DEV = `https://dev.scratcher.verse.bitcoin.com/`;
 
   const BASE_URL = isDev ? URL_DEV : URL_PROD;
   const query = new URLSearchParams();
@@ -10,7 +12,7 @@ export const generateScratcherUrl = (isWallet: boolean, campaign?: string) => {
   if (campaign) query.append("campaign", campaign);
   if (isWallet) query.append("origin", "wallet");
 
-  return `${BASE_URL}/?${query.toString()}`;
+  return `${BASE_URL}?${query.toString()}`;
 };
 
 export const generateFarmsUrl = (
@@ -18,15 +20,13 @@ export const generateFarmsUrl = (
   network: "eth",
   farm?: "verse-eth" | "usd-eth", // eg of farm path, we dont use in this project tho
 ) => {
-  const URL_PROD = `https://verse.bitcoin.com/farms`;
-  const URL_DEV = `https://dex.dev.cloud.bitcoin.com/farms`;
-
-  const BASE_URL = isDev ? URL_DEV : URL_PROD;
   const query = new URLSearchParams();
 
   if (isWallet) query.append("origin", "wallet");
 
-  return `${BASE_URL}/${network}/${farm ? `${farm}/` : ""}?${query.toString()}`;
+  return `${VERSE_BASE_URL}farms/${network}/${
+    farm ? `${farm}/` : ""
+  }?${query.toString()}`;
 };
 
 export const generateStakingUrl = (
@@ -34,45 +34,37 @@ export const generateStakingUrl = (
   network: "eth",
   coin: "verse",
 ) => {
-  const URL_PROD = `https://verse.bitcoin.com/staking`;
-  const URL_DEV = `https://dex.dev.cloud.bitcoin.com/staking`;
-
-  const BASE_URL = isDev ? URL_DEV : URL_PROD;
   const query = new URLSearchParams();
 
   if (isWallet) query.append("origin", "wallet");
 
-  return `${BASE_URL}/${coin}?${query.toString()}`;
+  return `${VERSE_BASE_URL}staking/${network}/${coin}?${query.toString()}`;
 };
 
 export const generateSwapUrl = (isWallet: boolean, coin: "verse") => {
-  const URL_PROD = `https://verse.bitcoin.com/swap`;
-  const URL_DEV = `https://dex.dev.cloud.bitcoin.com/swap`;
-
-  const BASE_URL = isDev ? URL_DEV : URL_PROD;
   const query = new URLSearchParams();
 
   query.append("coin", coin);
   if (isWallet) query.append("origin", "wallet");
 
-  return `${BASE_URL}/?${query.toString()}`;
+  return `${VERSE_BASE_URL}swap/?${query.toString()}`;
 };
 
 export const generateBuyUrl = (isWallet: boolean, coin: "verse") => {
   if (isWallet) {
-    const URL_DEEPLINK = `bitcoincom://buy`;
+    const URL_DEEPLINK = `bitcoincom://buy/`;
     const deeplinks = {
       verse:
         "ETH_BLOCKCHAIN-ERC_20_PROTOCOL-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18",
     };
 
-    return `${URL_DEEPLINK}/${deeplinks[coin]}`;
+    return `${URL_DEEPLINK}${deeplinks[coin]}`;
   }
 
-  const URL_PROD = `https://buy.bitcoin.com`;
-  const URL_DEV = `https://buy.dev.cloud.bitcoin.com`;
+  const URL_PROD = `https://buy.bitcoin.com/`;
+  const URL_DEV = `https://buy.dev.cloud.bitcoin.com/`;
 
   const BASE_URL = isDev ? URL_DEV : URL_PROD;
 
-  return `${BASE_URL}/${coin}`;
+  return `${BASE_URL}${coin}`;
 };
