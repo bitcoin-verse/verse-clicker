b8754e526aa0c617dd0bdb3e7e300e8f5d531774
Micah <micah@bitcoin.com>
2024-01-25 09:15:25 +0800

    [MTDB-12774] Capitalize word Scratcher

--- src/components/Boosts/Modals/ScratcherMint.tsx
+++ src/components/Boosts/Modals/ScratcherMint.tsx
@@ -16,19 +16,19 @@ const ScratcherMint: FC = () => {
   return (
     <ModalWrapper>
       <H3>
-        Boost your points production by buying Lunar New Year scratcher tickets
+        Boost your points production by buying Lunar New Year Scratcher tickets
       </H3>
       <Chip>Your current multiplier: {player.productionBase * 100}%</Chip>
       <Text>
         You will receive a 1% increase to your production rate multiplier for
-        every <b>Lunar New Year scratcher</b> ticket purchased
+        every <b>Lunar New Year Scratcher</b> ticket purchased
       </Text>
       <Text $secondary>
         NOTE: To receive a bonus, you must:
         <br />
         1. Purchase using the same address used here
         <br />
-        2. Scratcher purchased must be a Lunar New Year scratcher
+        2. Scratcher purchased must be a Lunar New Year Scratcher
       </Text>
       <WarningChip link="https://support.bitcoin.com/en/articles/8829750-verse-clicker-lunar-new-year-contest-faq">
         Tap here for detailed instructions
