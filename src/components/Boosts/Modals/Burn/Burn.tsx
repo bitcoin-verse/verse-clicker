fadc15bf1f9d9bd4d2ddac6114a29cd6114b3fae
Teh SoTo <sansoto5000@gmail.com>
2024-01-29 01:50:58 -0300

    fix(burn): :fire: Removes unused hooks

--- src/components/Boosts/Modals/Burn/Burn.tsx
+++ src/components/Boosts/Modals/Burn/Burn.tsx
@@ -1,17 +1,10 @@
 import React, { FC, useEffect, useState } from "react";
 import { formatEther } from "viem";
-import {
-  useAccount,
-  useContractWrite,
-  useNetwork,
-  useWaitForTransaction,
-} from "wagmi";
+import { useNetwork } from "wagmi";
 
 import verseIcon from "../../../../assets/verse-icon.png";
 import { useSocketCtx } from "../../../../context/SocketContext";
 import { useTrackedState } from "../../../../context/store";
-import getVerseTokenDetails from "../../../../contracts/getVerseTokenDetails";
-import { logAmplitudeEvent } from "../../../../helpers/analytics";
 import { formatNumber } from "../../../../helpers/formatNumber";
 import { getBurnEngineExplorerLink } from "../../../../helpers/getBurnEngineExplorerLink";
 import { generateBurnEngineUrl } from "../../../../helpers/links";
@@ -26,7 +19,6 @@ import PointsIcon from "../../../PointsIcon";
 import Tabs, { TabButton } from "../../../Tabs";
 import WarningChip from "../../../WarningChip";
 import { Divider, Footnote, Icon, ModalWrapper, Price } from "../../styled";
-import LoadingStates from "./LoadingStates";
 
 export const BURN_LIST = [
   { title: "1 hour", value: 10000, hours: 1 },
@@ -48,33 +40,16 @@ const calculateBurnBonus = (burnAmount: number) => {
 
 const Burn: FC = () => {
   const { socket } = useSocketCtx();
-  const { address } = useAccount();
   const { isWallet } = useTrackedState();
   const { chain } = useNetwork();
   const { data: readData, error } = useVerseBalance();
   const [newCookies, setNewCookies] = useState<number>();
   const { player } = useTrackedState();
-  const verseTokenDetails = getVerseTokenDetails(chain?.id);
 
   const [balanceData, setBalanceData] = useState<{
     formatted: string;
     value: bigint;
   }>();
-
-  const {
-    data: txData,
-    isLoading: isPendingWallet,
-    isSuccess: isTxSent,
-  } = useContractWrite({
-    address: verseTokenDetails?.address,
-    abi: verseTokenDetails?.abi,
-    functionName: "transfer",
-    chainId: chain?.id,
-  });
-
-  const { isSuccess: isTxConfirmed } = useWaitForTransaction(txData);
-
-  const [showLoading, setShowLoading] = useState(false);
 
   const [selectedTab, setSelectedTab] = useState(0);
   const selectedBurn = BURN_LIST[selectedTab];
@@ -89,28 +64,6 @@ const Burn: FC = () => {
   }, [readData, error]);
 
   useEffect(() => {
-    if (isPendingWallet || isTxSent || isTxConfirmed) {
-      setShowLoading(true);
-
-      logAmplitudeEvent({
-        name: "verse clicker burn",
-        "blockchain address": address || "",
-        blockchain: chain?.nativeCurrency.symbol,
-        result: selectedBurn.value,
-        txId: txData?.hash || "",
-      });
-    } else {
-      setShowLoading(false);
-    }
-  }, [isPendingWallet, isTxSent, isTxConfirmed]);
-
-  useEffect(() => {
-    if (txData) {
-      socket.emit("burn");
-    }
-  }, [txData]);
-
-  useEffect(() => {
     const onBonus = (data: number) => {
       setNewCookies(data);
     };
@@ -123,100 +76,86 @@ const Burn: FC = () => {
 
   return (
     <ModalWrapper>
-      {showLoading ? (
-        <LoadingStates
-          isPendingWallet={isPendingWallet}
-          isTxSent={isTxSent}
-          isTxConfirmed={isTxConfirmed}
-          newCookies={newCookies}
-          selectedTab={selectedTab}
-          txHash={txData?.hash}
-        />
-      ) : (
-        <>
-          <H3>Contribute VERSE to get an instant boost</H3>
-          <Label>
-            View the <BurnEngineLink /> for more information
-          </Label>
+      <>
+        <H3>Contribute VERSE to get an instant boost</H3>
+        <Label>
+          View the <BurnEngineLink /> for more information
+        </Label>
 
-          <Container>
-            <Label $color="secondary">Boost duration</Label>
-            <Tabs
-              center
-              mobileVersion
-              tabs={BURN_LIST.map((button, i) => (
-                <TabButton
-                  key={i}
-                  $mobileVersion
-                  $isSelected={selectedTab === i}
-                  type="button"
-                  onClick={() => setSelectedTab(i)}
-                >
-                  {button.title}
-                </TabButton>
-              ))}
-            />
-            <Label $color="secondary">Quantity required</Label>
-            <Price>
-              <Icon src={verseIcon} />
-              <Label $color={insufficientVerse ? "warning" : undefined}>
-                {selectedBurn.value.toLocaleString()} VERSE
-              </Label>
-            </Price>
-            <Label $color="secondary">Points you will receive</Label>
-
-            <Price>
-              <Label $color={insufficientVerse ? "warning" : undefined}>
-                {formatNumber(
-                  player.cps * calculateBurnBonus(selectedBurn.value),
-                )}{" "}
-              </Label>
-              <PointsIcon size="0.75rem" />
-            </Price>
-            <Divider />
-            <Label $color="secondary">
-              Available:{" "}
-              {balanceData?.formatted
-                ? Number(balanceData.formatted).toLocaleString()
-                : "0.00"}{" "}
-              VERSE
-            </Label>
-          </Container>
-          {insufficientVerse && (
-            <WarningChip
-              link={
-                isWallet
-                  ? "bitcoincom://buy/ETH_BLOCKCHAIN-ERC_20_PROTOCOL-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18"
-                  : `https://buy.bitcoin.com/verse/`
-              }
-            >
-              You don&#39;t have enough VERSE. Buy now!
-            </WarningChip>
-          )}
-
-          {!insufficientVerse && (
-            <LinkButton
-              href={generateBurnEngineUrl(isWallet)}
-              newTab={!isWallet}
-            >
-              Burn VERSE
-            </LinkButton>
-          )}
-          <Footnote>
-            <Label $color="secondary">
-              This transaction will send the specified VERSE to{" "}
-              <Link
-                href={getBurnEngineExplorerLink(chain?.id || 1)}
-                target="_blank"
-                rel="noreferrer"
+        <Container>
+          <Label $color="secondary">Boost duration</Label>
+          <Tabs
+            center
+            mobileVersion
+            tabs={BURN_LIST.map((button, i) => (
+              <TabButton
+                key={i}
+                $mobileVersion
+                $isSelected={selectedTab === i}
+                type="button"
+                onClick={() => setSelectedTab(i)}
               >
-                this contract
-              </Link>
-              , making the sent VERSE permanently inaccessible.
+                {button.title}
+              </TabButton>
+            ))}
+          />
+          <Label $color="secondary">Quantity required</Label>
+          <Price>
+            <Icon src={verseIcon} />
+            <Label $color={insufficientVerse ? "warning" : undefined}>
+              {selectedBurn.value.toLocaleString()} VERSE
             </Label>
-          </Footnote>
-        </>
-      )}
+          </Price>
+          <Label $color="secondary">Points you will receive</Label>
+
+          <Price>
+            <Label $color={insufficientVerse ? "warning" : undefined}>
+              {formatNumber(
+                player.cps * calculateBurnBonus(selectedBurn.value),
+              )}{" "}
+            </Label>
+            <PointsIcon size="0.75rem" />
+          </Price>
+          <Divider />
+          <Label $color="secondary">
+            Available:{" "}
+            {balanceData?.formatted
+              ? Number(balanceData.formatted).toLocaleString()
+              : "0.00"}{" "}
+            VERSE
+          </Label>
+        </Container>
+        {insufficientVerse && (
+          <WarningChip
+            link={
+              isWallet
+                ? "bitcoincom://buy/ETH_BLOCKCHAIN-ERC_20_PROTOCOL-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18"
+                : `https://buy.bitcoin.com/verse/`
+            }
+          >
+            You don&#39;t have enough VERSE. Buy now!
+          </WarningChip>
+        )}
+
+        {!insufficientVerse && (
+          <LinkButton href={generateBurnEngineUrl(isWallet)} newTab={!isWallet}>
+            Burn VERSE
+          </LinkButton>
+        )}
+        <Footnote>
+          <Label $color="secondary">
+            This transaction will send the specified VERSE to{" "}
+            <Link
+              href={getBurnEngineExplorerLink(chain?.id || 1)}
+              target="_blank"
+              rel="noreferrer"
+            >
+              this contract
+            </Link>
+            , making the sent VERSE permanently inaccessible.
+          </Label>
+        </Footnote>
+      </>
     </ModalWrapper>
   );
 };
