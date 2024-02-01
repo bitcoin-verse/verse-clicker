f242d10507278be50e9fd15a5149413153e34863
Paul Bergamo <porlybe@gmail.com>
2024-02-01 10:31:27 +0900

    setting up env vars

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,21 @@
 /// <reference types="react-scripts" />
 
 declare module ".*wav";
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    PUBLIC_URL: string;
+    REACT_APP_PUBLIC_URL: string;
+    REACT_APP_VERSE_BASE_URL: string;
+    REACT_APP_DEV_ENV: string;
+    REACT_APP_GOERLI_NODE_HTTP_URL: string;
+    REACT_APP_GOERLI_NODE_WSS_URL: string;
+    REACT_APP_SEPOLIA_NODE_HTTP_URL: string;
+    REACT_APP_SEPOLIA_NODE_WSS_URL: string;
+    REACT_APP_POLYGON_NODE_HTTP_URL: string;
+    REACT_APP_POLYGON_NODE_WSS_URL: string;
+    REACT_APP_ETHEREUM_NODE_HTTP_URL: string;
+    REACT_APP_ETHEREUM_NODE_WSS_URL: string;
+  }
+}
