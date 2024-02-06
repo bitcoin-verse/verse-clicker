7413c8c06c2952c78d265e38a38e577d38dd50f4
PorlyBe <porlybe@gmail.com>
2024-02-06 12:04:29 +0900

    create helper to get chain id for gamemode

--- src/helpers/gameMode.ts
+++ src/helpers/gameMode.ts
@@ -9,6 +9,23 @@ const gameModes: Record<string, GameMode> = {
   lunarnewyear: "LunarNewYear",
 };
 
+const gameModeDefaultChain: Record<string, number> = {
+  christmas: 137,
+  ethereum: 1,
+  polygon: 137,
+  goerli: 5,
+  sepolia: 11155111,
+  lunarnewyear: 137,
+};
+
+export const getGameModeDefaultChain = (query: string | null) => {
+  if (!query) return gameModeDefaultChain.ethereum;
+
+  const sanitized = query.replaceAll("-", "").toLowerCase();
+
+  return gameModeDefaultChain?.[sanitized] || gameModeDefaultChain.ethereum;
+};
+
 export const getGameMode = (query: string | null) => {
   if (!query) return gameModes.ethereum;
 
