2bd389ad16a5d4ada056f198da83b55665d7927f
Teh SoTo <sansoto5000@gmail.com>
2024-02-21 23:06:36 -0300

    chore(liteMode): :zap: Just sample and types update

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,7 +5,7 @@ declare module ".*wav";
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
-    LITE_MODE: "true" | "false";
+    REACT_APP_LITE_MODE: "true" | "false";
     PUBLIC_URL: string;
     REACT_APP_PUBLIC_URL: string;
     REACT_APP_VERSE_BASE_URL: string;
