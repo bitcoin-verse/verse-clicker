5e1f5b81e28ee9c6d7f650352c44ec9f02748d88
PorlyBe <porlybe@gmail.com>
2024-04-01 10:53:49 +0900

    handling navigation if history cleared

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useTrackedState } from "src/context/store";
 
 import verseIcon from "../../assets/verse-icon.png";
 import verseLogo from "../../assets/verse-logo.png";
@@ -15,12 +16,33 @@ import {
   Title,
 } from "./styled";
 
+const VERSE_BASE_URL =
+  process.env.REACT_APP_VERSE_BASE_URL || "https://verse.bitcoin.com/";
+
 const Header: FC = () => {
   const navigate = useNavigate();
 
+  const { pathname } = useLocation();
+  const { isWallet, gameMode } = useTrackedState();
+
   return (
     <StyledHeader>
-      <LogoWrapper onClick={() => navigate(-1)}>
+      <LogoWrapper
+        onClick={() => {
+          const search = new URLSearchParams();
+          if (isWallet) {
+            search.set("origin", "wallet");
+          }
+
+          if (pathname === "/") {
+            search.set("campaign", gameMode);
+            window.location.href = `${VERSE_BASE_URL}/?${search.toString()}`;
+          } else {
+            search.set("campaign", gameMode);
+            navigate(`/?${search.toString()}`);
+          }
+        }}
+      >
         <ChevronLeft />
         <Icon src={verseIcon} alt="Icon" />
         <Logo src={verseLogo} alt="Logo" />
